727a2fdc0dcbf968546f1d2de8b543787231ce59
Diptesh Saha <dipteshsahasaha@gmail.com>
2022-12-20 11:00:25 +0000

    pythonchanges

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+# Implement a Python decorator that should take whatever the decorated function returns, and write it to a file in a new line. For the sake of this problem, let us assume that the decorated functions always return a string. The decorator should be named log_message and should write to the file /tmp/decorator_logs.txt.
+
+# Objective: To develop a Python decorator
+
+# Domain:  Web Development
+#Name Diptesh Saha
+""""code starts from here """""
 def log_message(diptesh): 
     def simple(*args,**saha):
         output = diptesh(*args,**saha)
